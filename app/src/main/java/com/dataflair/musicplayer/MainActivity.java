b90451287e01d192f936b9b28252ea12e207adff
chinmayparekh <chinmayparekh11@gmail.com>
2022-01-23 12:12:51 +0530

    Adding music player

--- app/src/main/java/com/dataflair/musicplayer/MainActivity.java
+++ app/src/main/java/com/dataflair/musicplayer/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
                         displaySong();
+
                     }
 
                     @Override
